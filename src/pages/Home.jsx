fbf17f71bacf40e1dd4862e95e13c1a250515de0
Cesar Cordoba <cordobacesar29@gmail.com>
2024-02-27 18:02:08 -0300

    Customizer page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
     <AnimatePresence>
       {snap.intro && (
         <motion.section className="home" {...slideAnimation("left")}>
-          <motion.header>
+          <motion.header {...slideAnimation("down")}>
             <img
               src="./threejs.png"
               alt="logo"
@@ -27,24 +27,23 @@ const Home = () => {
           <motion.div className="home-content" {...headContainerAnimation}>
             <motion.div {...headTextAnimation}>
               <h1 className="head-text">
-                {" "}
                 Let's <br /> do it
               </h1>
             </motion.div>
-          </motion.div>
-          <motion.div {...headContentAnimation} className="flex flex-col gap-5">
-            <p className="max-w-md font-normal text-gray-600 text-base">
-              Create your unique and exclusive shirt with our brand-new 3D
-              customization tool. <strong>Unleash your imagination</strong> and
-              define your own style.
-            </p>
+            <motion.div {...headContentAnimation} className="flex flex-col gap-5">
+              <p className="max-w-md font-normal text-gray-600 text-base">
+                Create your unique and exclusive shirt with our brand-new 3D
+                customization tool. <strong>Unleash your imagination</strong> and
+                define your own style.
+              </p>
 
-            <CustomButton
-              type='filled'
-              title='Customize It'
-              handleClick={()=> state.intro = false}
-              customStyles='w-fit px-4 py-2.5 font-bold text-sm'
-            />
+              <CustomButton
+                type="filled"
+                title="Customize It"
+                handleClick={() => (state.intro = false)}
+                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+              />
+            </motion.div>
           </motion.div>
         </motion.section>
       )}
